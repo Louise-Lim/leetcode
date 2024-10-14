8107d57f34ef4c13a30a72328aeadbe32e5200f5
DESKTOP-G2HQ79N\ASUS <vylinglim2@gmail.com>
2024-10-14 15:49:16 +0800

    commit 80

---
+++ Array/RemoveDuplicateArray80.java
@@ -0,0 +1,33 @@
+package Array;
+
+import java.util.Arrays;
+
+public class RemoveDuplicateArray80 {
+    public static void main(String[] args) {
+        int[] nums = { 0, 0, 1, 1, 1, 1, 2, 2, 2, 3 };
+
+        RemoveDuplicateArrayII removeDuplicateArrayII = new RemoveDuplicateArrayII();
+        removeDuplicateArrayII.removeDuplicates(nums);
+    }
+}
+
+class RemoveDuplicateArrayII {
+    public int removeDuplicates(int[] nums) {
+        int k = 0;
+
+        for (int n : nums) {
+            if (k < 2 || n != nums[k - 2]) {
+                nums[k++] = n;
+            }
+        }
+
+        int[] temp = new int[k];
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[i];
+        }
+
+        System.out.println(k + " , " + Arrays.toString(temp));
+
+        return k;
+    }
+}
