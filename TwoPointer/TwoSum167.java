34434d8390b474bb0a63bab3c0284549e8a29a2d
DESKTOP-G2HQ79N\ASUS <vylinglim2@gmail.com>
2024-12-11 08:53:39 +0800

    Two Pointer 167

---
+++ TwoPointer/TwoSum167.java
@@ -0,0 +1,35 @@
+package TwoPointer;
+
+import java.util.Arrays;
+
+public class TwoSum167 {
+    public static void main(String[] args) {
+        TwoSumII twoSumII = new TwoSumII();
+        int[] nums = { 2, 7, 11, 15 };
+        int[] result = twoSumII.twoSum(nums, 9);
+        System.out.println(Arrays.toString(result));
+    }
+}
+
+class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int indexL = 0;
+        int indexR = numbers.length - 1;
+
+        while (indexL < indexR) {
+            int sum = numbers[indexL] + numbers[indexR];
+            
+            if (sum == target) {
+                return new int[] { indexL + 1, indexR + 1 }; // index need start from 1
+            }
+
+            if (sum > target) {
+                indexR--;
+            } else {
+                indexL++;
+            }
+        }
+
+        return numbers;
+    }
+}
