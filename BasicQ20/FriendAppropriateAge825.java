7818375d66e9a42516b02c1a8f116eef969db577
DESKTOP-G2HQ79N\ASUS <vylinglim2@gmail.com>
2025-08-24 23:45:28 +0800

    Basic Q20 update

--- BasicQ20/FriendAppropriateAge825.java
+++ BasicQ20/FriendAppropriateAge825.java
@@ -9,25 +9,20 @@ public class FriendAppropriateAge825 {
 
     public int numFriendRequest(int[] ages) {
         int[] counts = new int[121];
-        for (int age : ages) {
-            counts[age]++;
-        }
-
+        for (int age : ages) counts[age]++;
+        
         int totalRequests = 0;
-        // 遍历所有可能的发送者年龄 ageX
-        // 从 15 岁开始循环，因为 14 岁及以下的人不会发送请求 (见上面的优化说明)。
         for (int ageX = 15; ageX <= 120; ageX++) {
-            if (counts[ageX] == 0) {
-                continue;
-            }
+            
+            if (counts[ageX] == 0) continue;
 
-            for (int ageY = 1; ageY <= 120; ageY++) {
-                if (counts[ageY] == 0) {
-                    continue;
-                }
+            int min = (int)(0.5 * ageX + 7);
+            for (int ageY = min + 1; ageY <= ageX; ageY++) {
+                
+                if (counts[ageY] == 0) continue;
 
                 // Condtion
-                if (!(ageY <= 0.5 * ageX + 7 || ageY > ageX || (ageY > 100 && ageX < 100))) {
+                if (!(ageY <= 0.5 * ageX + 7 || (ageY > 100 && ageX < 100))) {
                     // ageX send request to ageY
                     if (ageX == ageY) {
                         totalRequests = totalRequests + (counts[ageX] * (counts[ageX] - 1));
